41476817c185cea1fa5246cdb9c7541a664aee21
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-23 00:01:45 +0100

    fixed typo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Switcher from "./switcher";
 
 function App() {
   return (
-    <div className="ransition duration-1000 ease-out bg-blue-300 dark:bg-slate-700">
+    <div className="transition duration-1000 ease-out bg-blue-300 dark:bg-slate-700">
       <h1 className="text-3xl bg-slate-400 font-bold underline">
         Hello world!
       </h1>
