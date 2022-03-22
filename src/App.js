92fbbdcccf0b113f19670519ef1e41bd77eae37a
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-22 23:22:57 +0100

    added transition effect to the light and dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Switcher from "./switcher";
 
 function App() {
   return (
-    <h1 className="text-3xl bg-slate-400 font-bold underline">Hello world!</h1>
+    <div className="ransition duration-1000 ease-out bg-blue-300 dark:bg-slate-700">
+      <h1 className="text-3xl bg-slate-400 font-bold underline">
+        Hello world!
+      </h1>
+      <Switcher />
+    </div>
   );
 }
 
