d65b25d167b2677ae988deb989d2fd64f5c4ae5d
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-24 22:03:25 +0100

    added nav items

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Project</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Navbar;
