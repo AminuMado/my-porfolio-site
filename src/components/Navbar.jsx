98deab70dc16185df2ca918e11b80a4465292d63
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-25 17:13:45 +0100

    removed margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 function Navbar() {
   return (
-    <header className="flex  justify-center items-center m-2 p-2">
+    <header className="flex justify-center items-center p-2">
       <nav>
-        <ul className="flex justify-center items-center gap-10 text-xl  ">
-          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105 ">
+        <ul className="flex justify-center items-center gap-10 text-xl">
+          <li className="cursor-pointer transition-all duration-300 hover:text-#05204A  active:scale-105">
             Home
           </li>
           <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
