48425753230f802c6cbb7687218c108b215b13a9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-25 20:24:35 +0100

    added link routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <header className="flex justify-center items-center p-2">
       <nav>
         <ul className="flex justify-center items-center gap-10 text-xl">
-          <li className="cursor-pointer transition-all duration-300 hover:text-#05204A  active:scale-105">
-            Home
-          </li>
-          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
-            About
-          </li>
-          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
-            Project
-          </li>
-          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
-            Contact
-          </li>
+          <Link to="/">
+            <li className="cursor-pointer transition-all duration-300 hover:text-#05204A  active:scale-105">
+              Home
+            </li>
+          </Link>
+          <Link to="/About">
+            <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
+              About
+            </li>
+          </Link>
+          <Link to="/Project">
+            <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
+              Project
+            </li>
+          </Link>
+          <Link to="/Contact">
+            <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
+              Contact
+            </li>
+          </Link>
         </ul>
       </nav>
     </header>
