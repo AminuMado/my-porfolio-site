35177ac94e21edfa18a83b373b426b63d2d5f9ea
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-25 07:04:11 +0100

    centered the nav bar and added hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 function Navbar() {
   return (
-    <header>
+    <header className="flex  justify-center items-center m-2 p-2">
       <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Project</li>
-          <li>Contact</li>
+        <ul className="flex justify-center items-center gap-10 text-xl  ">
+          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105 ">
+            Home
+          </li>
+          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
+            About
+          </li>
+          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
+            Project
+          </li>
+          <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
+            Contact
+          </li>
         </ul>
       </nav>
     </header>
