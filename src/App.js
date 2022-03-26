0f839910f5ec571dfc30eab7272e3b193c7d9d96
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-26 06:46:54 +0100

    moved the switcher component outside the route

--- src/App.js
+++ src/App.js
@@ -5,30 +5,39 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
+import Switcher from "./switcher";
 
 function App() {
   const [mode, setMode] = React.useState("light");
 
   return (
-    <BrowserRouter basename="/">
-      <React.Fragment>
-        <Routes>
-          <Route path="/" element={<Home mode={mode} setMode={setMode} />} />
-          <Route
-            path="/About"
-            element={<About mode={mode} setMode={setMode} />}
-          />
-          <Route
-            path="/Project"
-            element={<Project mode={mode} setMode={setMode} />}
-          />
-          <Route
-            path="/Contact"
-            element={<Contact mode={mode} setMode={setMode} />}
-          />
-        </Routes>
-      </React.Fragment>
-    </BrowserRouter>
+    <div>
+      <Switcher setMode={setMode} />
+
+      <BrowserRouter basename="/">
+        <React.Fragment>
+          <Routes>
+            <Route path="/" element={<Home mode={mode} setMode={setMode} />} />
+            <Route
+              path="/About"
+              element={
+                <>
+                  <About mode={mode} setMode={setMode} />
+                </>
+              }
+            />
+            <Route
+              path="/Project"
+              element={<Project mode={mode} setMode={setMode} />}
+            />
+            <Route
+              path="/Contact"
+              element={<Contact mode={mode} setMode={setMode} />}
+            />
+          </Routes>
+        </React.Fragment>
+      </BrowserRouter>
+    </div>
   );
 }
 
