9e6d94c82b149b287ccabd7ff2ff9ef351cf4bef
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-27 21:39:43 +0100

    add new font style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,12 @@ function Home({ mode }) {
     before:bg-green-400 before:h-80 before:w-screen before:absolute before:bottom-0"
     >
       <Navbar />
+      <h1 className="font-SpaceGrotesk text-8xl">
+        Hi! <br /> I'm <b>Aminu Mukhtar</b>.<br />A front-end web developer.
+      </h1>
+      <p className="font-Karma text-2xl">
+        Sometimes I create stuff. Other times I just code stuff.
+      </p>
       <Footer />
     </div>
   );
