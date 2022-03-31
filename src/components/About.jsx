13f197ba58a3d231773f2311c8cd0f3a91377b9c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-31 16:24:55 +0100

    add devicons and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,90 @@ import Footer from "./Footer";
 
 function About({ mode, setMode }) {
   return (
-    <div className="flex flex-col items-center justify-center transition duration-500 ease-out bg-slate-50 dark:bg-black dark:text-white h-screen">
+    <div className="flex flex-col items-center justify-center transition duration-500 ease-out bg-neutral-100 dark:bg-black dark:text-white h-screen">
       <Navbar />
       <Footer />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-        className="absolute bottom-0 z-10 "
-      >
-        <path
-          fill={mode === "light" ? "#C5EFFC" : "#001429"}
-          fillOpacity="1"
-          d="M0,224L20,213.3C40,203,80,181,120,170.7C160,160,200,160,240,181.3C280,203,320,245,360,224C400,203,440,117,480,90.7C520,64,560,96,600,101.3C640,107,680,85,720,64C760,43,800,21,840,21.3C880,21,920,43,960,90.7C1000,139,1040,213,1080,240C1120,267,1160,245,1200,240C1240,235,1280,245,1320,213.3C1360,181,1400,107,1420,69.3L1440,32L1440,320L1420,320C1400,320,1360,320,1320,320C1280,320,1240,320,1200,320C1160,320,1120,320,1080,320C1040,320,1000,320,960,320C920,320,880,320,840,320C800,320,760,320,720,320C680,320,640,320,600,320C560,320,520,320,480,320C440,320,400,320,360,320C320,320,280,320,240,320C200,320,160,320,120,320C80,320,40,320,20,320L0,320Z"
-        ></path>
-      </svg>
+      <main className="flex flex-wrap justify-around items-center font-Karma m-10 p-10">
+        <div className="min-w-[400px] w-1/2 p-4 leading-loose text-justify text-xl">
+          <p>
+            Growing up, I always had an affinity for tech and computers. Coming
+            from a background of continuous practice and performance I was
+            immediately enthralled by the similarity in development in creating
+            new products and experiences in a satisfyingly tangible manner.
+            Committed to the idea of life-long learning, I am a full stack
+            developer with a passion for Javascript, React, and all things web
+            development. And as a follower of John Maeda’s laws of simplicity, I
+            agree that less is more.
+          </p>
+          <p>Outisde of development, I enjoy playing chess and basketball.</p>
+        </div>
+        <div className="flex flex-wrap items-center justify-between max-w-2xl w-96 font-Karma ">
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-html5-plain colored icon "></i>
+            <span className="text-sm">HTML</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-css3-plain colored icon"></i>
+            <span className="text-sm">CSS</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-javascript-plain colored icon"></i>
+            <span className="text-sm">JAVASCRIPT</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-typescript-plain colored icon"></i>
+            <span className="text-sm">TYPESCRIPT</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-sass-original colored icon"></i>
+            <span className="text-sm">SASS</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className=" text-5xl basis-1/5 devicon-tailwindcss-original-wordmark colored"></i>
+            <span className="text-sm">TAILWIND</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-react-original colored icon"></i>
+            <span className="text-sm">REACT</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-redux-original colored icon"></i>
+            <span className="text-sm">REDUX</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-mongodb-plain colored icon"></i>
+            <span className="text-sm">MONGODB</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-express-original colored icon express"></i>
+            <span className="text-sm">EXPRESS</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-nodejs-plain colored icon"></i>
+            <span className="text-sm">NODEJS</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-jest-plain colored icon"></i>
+            <span className="text-sm">JEST</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-git-plain colored icon"></i>
+            <span className="text-sm">GIT</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-webpack-plain colored icon"></i>
+            <span className="text-sm">WEBPACK</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-heroku-plain colored icon"></i>
+            <span className="text-sm">HEROKU</span>
+          </div>
+          <div className="flex flex-col m-3 w-16 text-center items-center justify-center gap-2">
+            <i className="text-5xl basis-1/5 devicon-npm-original-wordmark colored icon"></i>
+            <span className="text-sm">NPM</span>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
