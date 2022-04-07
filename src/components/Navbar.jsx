e9d4ffe4b06076f516bdf86d888f0c81a36ac671
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-07 10:57:26 +0100

    refactored to remove duplicate code and added logic for active page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ active }) {
+  const styles = {
+    default:
+      "cursor-pointer transition-all duration-75 hover:text-green-400 active:scale-95",
+    active: "text-blue-300",
+  };
+  const navList = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/About" },
+    { name: "Project", link: "/Project" },
+    { name: "Contact", link: "/Contact" },
+  ];
+  const navItems = navList.map((item) => {
+    return (
+      <Link to={item.link} key={item.name}>
+        <li className={active === item.name ? styles.active : styles.default}>
+          {item.name}
+        </li>
+      </Link>
+    );
+  });
   return (
     <header className="absolute left-1/3/2 top-1 flex justify-center items-center p-2">
       <nav>
         <ul className="flex justify-center items-center gap-10 text-xl">
-          <Link to="/">
-            <li className="cursor-pointer transition-all duration-300 hover:text-green-400  active:scale-105">
-              Home
-            </li>
-          </Link>
-          <Link to="/About">
-            <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
-              About
-            </li>
-          </Link>
-          <Link to="/Project">
-            <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
-              Project
-            </li>
-          </Link>
-          <Link to="/Contact">
-            <li className="cursor-pointer transition-all duration-300 hover:text-purple-700  active:scale-105">
-              Contact
-            </li>
-          </Link>
+          {navItems}
         </ul>
       </nav>
     </header>
